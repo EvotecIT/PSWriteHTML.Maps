f9525259d40326ad4c4ce756906649bb47c626d0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-06 17:33:51 +0100

    Functions

---
+++ Private/Parameters.Configuration.ps1
@@ -0,0 +1,43 @@
+﻿$Script:ConfigurationURL = 'https://cdn.jsdelivr.net/gh/evotecit/cdn@0.0.6'
+$Script:Configuration = [ordered] @{
+    JQueryMouseWheel      = @{
+        Header      = @{
+            JSLink = 'https://cdn.jsdelivr.net/npm/jquery-mousewheel@3.1.13/jquery.mousewheel.min.js'
+            JS     = "$PSScriptRoot\..\Resources\JS\jquery.mousewheel.min.js"
+        }
+        SourceCodes = 'https://github.com/jquery/jquery-mousewheel'
+        License     = 'MIT'
+        LicenseLink = 'https://github.com/jquery/jquery-mousewheel/blob/master/LICENSE.txt'
+        Default     = $true
+        Email       = $false
+    }
+    Mapael                = @{
+        Comment     = 'Mapael JQuery'
+        Header      = @{
+            JSLink = 'https://cdn.jsdelivr.net/npm/jquery-mapael@2.2.0/js/jquery.mapael.min.js'
+            JS     = "$PSScriptRoot\..\Resources\JS\jquery.mapael.min.js"
+        }
+        Library     = 'https://github.com/neveldo/jQuery-Mapael'
+        SourceCodes = 'https://github.com/neveldo/jQuery-Mapael'
+        License     = 'MIT'
+        LicenseLink = 'https://github.com/neveldo/jQuery-Mapael/blob/master/LICENSE'
+        Default     = $true
+        Email       = $false
+    }
+    MapaelMaps_Poland     = @{
+        Header  = @{
+            JSLink = 'https://cdn.jsdelivr.net/gh/evotecit/cdn@0.0.7/Maps/poland/poland.min.js'
+            JS     = "$PSScriptRoot\..\Resources\Maps\jquery.mapael.poland.min.js"
+        }
+        Default = $true
+        Email   = $false
+    }
+    MapaelMaps_usa_states = @{
+        Header  = @{
+            JSLink = 'https://cdn.jsdelivr.net/gh/evotecit/cdn@0.0.7/Maps/usa/usa_states.min.js'
+            JS     = "$PSScriptRoot\..\Resources\JS\jquery.mapael.usa_states.min.js"
+        }
+        Default = $true
+        Email   = $false
+    }
+}
---
+++ Public/New-HTMLMap.ps1
@@ -0,0 +1,28 @@
+﻿function New-HTMLMap {
+    [cmdletBinding()]
+    param(
+        [parameter(Mandatory)][ValidateSet('poland', 'usa_states')][string] $Map,
+        [string] $AnchorName
+    )
+    Enable-HTMLFeature -Feature Raphael, Mapael, Jquery, JQueryMouseWheel, "MapaelMaps_$Map" -Configuration $Script:Configuration
+
+    if (-not $AnchorName) {
+        $AnchorName = "MapContainer$(Get-RandomStringName -Size 8)"
+    }
+    $Options = @{
+        map = @{
+            name = $Map.ToLower()
+        }
+    }
+    $OptionsJSON = $Options | ConvertTo-JsonLiteral -Depth 2
+
+    New-HTMLTag -Tag 'script' {
+        "`$(function () { `$(`".$AnchorName`").mapael($OptionsJSON); });"
+    }
+
+    New-HTMLTag -Tag 'div' -Attributes @{ class = $AnchorName } {
+        New-HTMLTag -Tag 'div' -Attributes @{ class = 'map' } {
+            $AlternateHTML
+        }
+    }
+}
