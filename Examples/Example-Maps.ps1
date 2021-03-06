5b776393ff80966f0a299fae9cf140f38890039d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-06 17:54:15 +0100

    Maps example

---
+++ Examples/Example-Maps.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module 'C:\Support\GitHub\PSWriteHTML\PSWriteHTML.psd1' -Force
+Import-Module .\PSWriteHTML.Maps.psd1 -Force
+
+New-HTML {
+    New-HTMLSection -Invisible {
+        New-HTMLPanel {
+            New-HTMLMap -Map poland
+        }
+        New-HTMLPanel {
+            New-HTMLMap -Map usa_states
+        }
+    }
+} -ShowHTML -Online -FilePath $PSScriptRoot\Example-Maps.html
