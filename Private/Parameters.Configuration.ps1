075e3ede798b6b172ead83704b85c170a1f416e9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-06 21:41:19 +0100

    Update libraries

--- Private/Parameters.Configuration.ps1
+++ Private/Parameters.Configuration.ps1
@@ -1,43 +1,56 @@
-﻿$Script:ConfigurationURL = 'https://cdn.jsdelivr.net/gh/evotecit/cdn@0.0.6'
+﻿$Script:ConfigurationURL = 'https://cdn.jsdelivr.net/gh/evotecit/cdn@0.0.8'
 $Script:Configuration = [ordered] @{
-    JQueryMouseWheel      = @{
-        Header      = @{
-            JSLink = 'https://cdn.jsdelivr.net/npm/jquery-mousewheel@3.1.13/jquery.mousewheel.min.js'
-            JS     = "$PSScriptRoot\..\Resources\JS\jquery.mousewheel.min.js"
+    Features = @{
+        JQueryMouseWheel      = @{
+            Header      = @{
+                JSLink = 'https://cdn.jsdelivr.net/npm/jquery-mousewheel@3.1.13/jquery.mousewheel.min.js'
+                JS     = "$PSScriptRoot\..\Resources\JS\jquery.mousewheel.min.js"
+            }
+            SourceCodes = 'https://github.com/jquery/jquery-mousewheel'
+            License     = 'MIT'
+            LicenseLink = 'https://github.com/jquery/jquery-mousewheel/blob/master/LICENSE.txt'
+            Default     = $true
+            Email       = $false
         }
-        SourceCodes = 'https://github.com/jquery/jquery-mousewheel'
-        License     = 'MIT'
-        LicenseLink = 'https://github.com/jquery/jquery-mousewheel/blob/master/LICENSE.txt'
-        Default     = $true
-        Email       = $false
-    }
-    Mapael                = @{
-        Comment     = 'Mapael JQuery'
-        Header      = @{
-            JSLink = 'https://cdn.jsdelivr.net/npm/jquery-mapael@2.2.0/js/jquery.mapael.min.js'
-            JS     = "$PSScriptRoot\..\Resources\JS\jquery.mapael.min.js"
+        Mapael                = @{
+            Comment     = 'Mapael JQuery'
+            Header      = @{
+                JSLink = 'https://cdn.jsdelivr.net/npm/jquery-mapael@2.2.0/js/jquery.mapael.min.js'
+                JS     = "$PSScriptRoot\..\Resources\JS\jquery.mapael.min.js"
+            }
+            Library     = 'https://github.com/neveldo/jQuery-Mapael'
+            SourceCodes = 'https://github.com/neveldo/jQuery-Mapael'
+            License     = 'MIT'
+            LicenseLink = 'https://github.com/neveldo/jQuery-Mapael/blob/master/LICENSE'
+            Default     = $true
+            Email       = $false
         }
-        Library     = 'https://github.com/neveldo/jQuery-Mapael'
-        SourceCodes = 'https://github.com/neveldo/jQuery-Mapael'
-        License     = 'MIT'
-        LicenseLink = 'https://github.com/neveldo/jQuery-Mapael/blob/master/LICENSE'
-        Default     = $true
-        Email       = $false
-    }
-    MapaelMaps_Poland     = @{
-        Header  = @{
-            JSLink = 'https://cdn.jsdelivr.net/gh/evotecit/cdn@0.0.7/Maps/poland/poland.min.js'
-            JS     = "$PSScriptRoot\..\Resources\Maps\jquery.mapael.poland.min.js"
+        MapaelMaps_Poland     = @{
+            Internal = $true
+            Header   = @{
+                JSLink = "$($Script:ConfigurationURL)/Maps/poland/poland.min.js"
+                JS     = "$PSScriptRoot\..\Resources\Maps\poland\poland.js"
+            }
+            Default  = $true
+            Email    = $false
         }
-        Default = $true
-        Email   = $false
-    }
-    MapaelMaps_usa_states = @{
-        Header  = @{
-            JSLink = 'https://cdn.jsdelivr.net/gh/evotecit/cdn@0.0.7/Maps/usa/usa_states.min.js'
-            JS     = "$PSScriptRoot\..\Resources\JS\jquery.mapael.usa_states.min.js"
+        MapaelMaps_usa_states = @{
+            Internal = $true
+            Header   = @{
+                JSLink = "$($Script:ConfigurationURL)/Maps/usa/usa_states.min.js"
+                JS     = "$PSScriptRoot\..\Resources\Maps\usa\usa_states.js"
+            }
+            Default  = $true
+            Email    = $false
         }
-        Default = $true
-        Email   = $false
     }
 }
+
+<#
+$Keys = @(
+    'JQueryMouseWheel'
+    'Mapael'
+)
+
+Save-HTMLResource -Configuration $Script:Configuration -Keys $Keys -PathToSave 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSWriteHTML.MAPS\Resources' -Verbose
+#>
