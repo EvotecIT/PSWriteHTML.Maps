f1433ba5180b6eaf98d1056edbdd1776f56db6cf
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-07 16:19:47 +0100

    Example update

--- Examples/Example-Maps.ps1
+++ Examples/Example-Maps.ps1
@@ -10,4 +10,9 @@ New-HTML {
             New-HTMLMap -Map usa_states
         }
     }
+    New-HTMLSection -Invisible {
+        New-HTMLPanel {
+            New-HTMLMap -Map world_countries
+        }
+    }
 } -ShowHTML -Online -FilePath $PSScriptRoot\Example-Maps.html
